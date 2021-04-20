a85a362127429b152edcfbb70a3dc0d998e8178b
Connor Ness <35226859+ConnorNess@users.noreply.github.com>
2021-04-20 19:36:33 +0100

    authenticate changes
    make it actually personal to this project, lecturer probably gonna have a ton of the same name stuff

--- logauthenticate.php
+++ logauthenticate.php
@@ -6,7 +6,7 @@ session_start();
 $DATABASE_HOST = 'localhost';
 $DATABASE_USER = 'root';
 $DATABASE_PASS = '';
-$DATABASE_NAME = 'phplogin';
+$DATABASE_NAME = '40272321_cw2';
 
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if ( mysqli_connect_errno() ) {
